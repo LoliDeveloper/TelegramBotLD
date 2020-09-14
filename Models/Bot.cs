035e9c1ad902c6b54a698f8b0dedc2d67b57222a
LoliDeveloper <kiberghost79@gmail.com>
2020-09-14 12:59:59 +0500

    load existing rep

---
+++ Models/Bot.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TelegramBotLD.Models
+{
+    public class Bot
+    {
+    }
+}
---
+++ Models/Commands/StartCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TelegramBotLD.Models.Commands
+{
+    public class StartCommand : Command
+    {
+        public override string Name => @"/start";
+
+        public override async void Execute(Message message, TelegramBotClient client)
+        {
+            var chatId = message.Chat.Id;
+            var messageId = message.MessageId;
+
+            //TODO: Command logic -_-----
+
+            await client.SendTextMessageAsync(chatId, "Hello! I'm Schedule bot!\n*P.S* Not working yet", replyToMessageId: messageId);
+        }
+    }
+}
